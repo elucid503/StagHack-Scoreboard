cac88a1c5b172191c6d91303933e6a12c1cf712b
Elucid503 <paulujlaky@gmail.com>
2024-01-25 20:49:22 -0500

    Initial Files

---
+++ backend/index.ts
@@ -0,0 +1,164 @@
+// Scoreboard for the StagHack Competition, 2024
+
+import express from 'express';
+import cors from 'cors';
+
+import { readFile, writeFile } from 'fs/promises';
+
+import { port } from "./config.json"
+
+const app = express();
+
+app.use(express.static('../frontend/')); // For serving HTML files
+
+// Always send CORS headers
+
+app.use(cors());
+
+app.listen(port, () => {
+
+    console.log(`INFO: Server started on port ${port}`);
+
+});
+
+// Static routes
+
+app.get('/', (_req, res) => {
+
+    res.sendFile('./html/index.html', { root: '../frontend/' });
+
+});
+
+app.get('/api', (_req, res) => {
+
+    res.json({
+        Message: "Hello World!"
+    });
+
+});
+
+// Some helpful functions to read and write the score
+
+async function UpdateScore(NewScore: number): Promise<boolean> {
+
+    let data = await readFile("./score.json", "utf8").catch(err => {
+
+        console.log(`FILE ERR: ${err}`);
+        return null;
+
+    });
+
+    if (!data) return false;
+
+    let stored = JSON.parse(data);
+    stored.CurrentScore = NewScore;
+
+    await writeFile("./score.json", JSON.stringify(stored));
+
+    return true;
+
+}
+
+async function GetScore(): Promise<number> {
+
+    let data = await readFile("./score.json", "utf8").catch(err => {
+
+        console.log(`FILE ERR: ${err}`);
+        return null;
+
+    });
+
+    if (!data) return -1;
+
+    let stored = JSON.parse(data);
+    return stored.CurrentScore ?? -1; // ?? because || doesn't work for 0 (gotta love javascript!)
+
+}
+
+// Easy way to just get the number
+app.get('/api/score', async (_req, res) => {
+
+    let score = await GetScore();
+
+    res.json({
+        Score: score
+    });
+
+});
+
+// Can send a patch to easily increment the score
+app.patch('/api/score/:direction', async (req, res) => {
+
+    let score = await GetScore();
+    let direction: string | null = req.params.direction;
+
+    let NewScore = direction == "down" ? score - 1 : score + 1;
+
+    let success = await UpdateScore(NewScore);
+
+    if (success) {
+
+        res.json({
+            Score: NewScore
+        });
+
+    } else {
+
+        res.json({
+            Error: "Failed to update score"
+        });
+
+    }
+
+});
+
+// Can send a post with a score url-param to overwrite the score
+
+app.post('/api/score/:score', async (req, res) => {
+
+    let score = parseInt(req.params.score);
+
+    if (isNaN(score)) {
+
+        res.json({
+            Error: "Invalid number provided."
+        });
+
+        return;
+
+    }
+
+    let success = await UpdateScore(score);
+
+    if (success) {
+
+        res.json({
+            Score: score
+        });
+
+    } else {
+
+        res.json({
+            Error: "Failed to update score"
+        });
+
+    }
+
+});
+
+// Define a 404 route (after all other routes)
+
+app.use((_req, res) => {
+
+    res.status(404).json({
+
+        Error: "404: Not Found"
+
+    });
+
+});
+
+
+
+
+
---
+++ frontend/scripts/index.js
@@ -0,0 +1,123 @@
+const Routes = {
+
+    GetScore: { Url: "http://localhost:1337/api/score", Method: "GET", Args: 0 },
+    IncrementScore: { Url: "http://localhost:1337/api/score", Method: "PATCH", Args: 1 },
+    ChangeScore: { Url: "http://localhost:1337/api/score", Method: "POST", Args: 1 },
+
+}
+
+async function MakeRequestToBackend(Route, Argument = null){
+
+    const response = await fetch(`${Route.Url}${Argument ? `/${Argument}` : ""}`, {
+
+        method: Route.Method,
+        headers: { "Content-Type": "application/json" },
+
+    }).catch((error) => {
+
+        console.error(error);
+
+        console.log(`REQUEST ERR: ${error}`);
+
+    });
+
+    return await response?.json().catch((error) => {
+
+        console.log(`REQ PARSE ERR: ${error}`);
+
+    });
+
+}
+
+// Update with current score
+
+const Elements = {
+
+    Score: $("#score"),
+    Increment: $(".button.increment"),
+
+}
+
+function ValidateResponse(Response){
+
+    if (Response?.Error) {
+
+        Elements.Score.text(Response.Error);
+        return;
+
+    }
+
+    else if (Response.Score === undefined) {
+
+        Elements.Score.text("Error: Score is Unavailable");
+        return;
+
+    }
+
+    Elements.Score.text(Response.Score);
+
+}
+
+async function GetScore(){
+
+    const response = await MakeRequestToBackend(Routes.GetScore);
+
+    ValidateResponse(response);
+
+}
+
+async function IncrementScore(Direction = "Forward"){
+
+    const response = await MakeRequestToBackend(Routes.IncrementScore, Direction);
+
+    ValidateResponse(response);
+
+}
+
+// Not used, yet
+
+// async function ChangeScore(NewScore = 0){
+//
+//     const response = await MakeRequestToBackend(Routes.ChangeScore, NewScore);
+//
+//     if (response?.error) {
+//
+//         Elements.Score.text(response.Error);
+//         return;
+//
+//     }
+
+//     else if (!response?.Score) {
+
+//         Elements.Score.text("Error: Score is Unavailable");
+
+//         return;
+
+//     }
+//
+//     Elements.Score.text(response.Score);
+//
+// }
+
+// Update Score
+
+GetScore().then(() => {});
+
+// Event listeners
+Elements.Increment.each((index, element) => {
+
+    $(element).click(async (event) => {
+
+        const target = $(event.target);
+
+        await IncrementScore(target.attr("data-direction"));
+
+    });
+
+});
+
+
+
+
+
+
